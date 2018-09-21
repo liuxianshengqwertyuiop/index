19a660737f961ba46168c4c75ef72cebb0f04c8c
liuxianshengqwertyuiop <43464705+liuxianshengqwertyuiop@users.noreply.github.com>
2018-09-21 20:23:19 +0800

    去玩儿体育

---
+++ demo1.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    content = {
+        'username':'zhangsan',
+        'password':'password',
+        'age':12
+    }
+    return render_template("index.html",**content)
+
+@app.route("/sub/")
+def sub():
+    return render_template("sub.html")
+
+if __name__ == '__main__':
+    app.run()
